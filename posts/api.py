5562c9e012284c7f2c5ac8f5f59be96484d444c9
smedinacorey <s.medinacorey@gmail.com>
2016-12-25 16:08:30 +0000

    initial commit

--- posts/api.py
+++ posts/api.py
@@ -7,4 +7,45 @@ from . import models
 from . import decorators
 from posts import app
 from .database import session
+from .database import Post
 
+@app.route("/api/posts", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get():
+    """ Get a list of posts """
+    title_like = request.args.get("title_like")
+
+    posts = session.query(Post)
+    if title_like:
+        posts = posts.filter(Post.title.contains(title_like))
+    posts = posts.order_by(Post.id)
+
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+def post_get():
+    """ Single post endpoint """
+    post = session.query(Post).get(id)
+     
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+        
+    data = json.dumps(post.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/posts/<int:id>", methods=["DELETE"])
+def post_delete():
+    """ Delete a single post """
+    post = session.query(Post).get(id)
+    
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+    
+    data = json.dumps([])
+    return Response(data, 200, mimetype="application/json")
--- posts/database.py
+++ posts/database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Integer, DateTime, Text, Column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+import datetime
+
+from . import app
+
 
 from posts import app
 
@@ -8,3 +15,18 @@ engine = create_engine(app.config["DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+class Post(Base):
+    __tablename__ = "posts"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128))
+    body = Column(String(1024))
+    
+    def as_dictionary(self):
+        post = {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body
+        }
+        return post
